38b4f9ad22247ecbaf64d75573fdb2b4e3e43ee1
Tommy Ferritto <Tommy@28-cf-da-f2-cc-e4.wpa.txstate.edu>
2013-09-25 16:01:20 -0500

    Initial Commit

---
+++ Assignment 3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment 3
+//
+//  Created by Tommy Ferritto on 9/25/13.
+//  Copyright (c) 2013 Tommy Ferritto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
